40b418ef22c00e1f0e3310cc207c21aa22694f8d
Ken Coppola <mwave317@gmail.com>
2017-08-29 22:20:46 -0400

    Uploading files

---
+++ javascript/schema.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const snippetSchema = new mongoose.Schema({
+coder: {type: String, required: true},
+title: {type: String, required: true, unique: true},
+published_date: {type: Date, required: true},
+updated_date: {type: Date},
+body: {type: String, required: true},
+notes:{type: String, required: true},
+language: {type: String, required: true},
+rating: {type: Number},
+tags: {type: String, required: true},
+user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+});
+const userSchema = new mongoose.Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  reenterPassword: {type: String, required: true},
+  first_name: {type: String, required: true},
+  last_name: {type: String, required: true},
+  city: {type: String, required: true},
+  state: {type: String, required: true},
+});
+const Snippet = mongoose.model('Snippet', snippetSchema);
+const User = mongoose.model('User', userSchema);
+// let snippet = new Snippet({
+//   coder: "Ken Coppola",
+//   title: "Add Two Numbers",
+//   published_date: 8/25/17,
+//   body: "function addTwoNumbers(a,b){return a + b;};",
+//   notes: "This function will add two numbers",
+//   language: "javascript",
+//   rating: 1,
+//   tags: ["javascript"],
+//   user: "ken", ref: "ken",
+// });
+//
+// let user = new User({
+//   username: "ken",
+//   password: "asd",
+//   first_name: "Ken",
+//   last_name: "Coppola",
+//   city: "Waxhaw",
+//   state: "NC",
+// });
+ // snippet.user = user;
+// snippet.save();
+// user.save();
+
+module.exports = {
+  Snippet: Snippet,
+  User: User
+};
---
+++ javascript/serverside.js
@@ -0,0 +1,210 @@
+const express = require('express');
+const mustache = require('mustache-express');
+const Snippet = require('./schema.js').Snippet;
+const User = require('./schema.js').User;
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const session = require('express-session');
+mongoose.connect('mongodb://localhost:27017/snippets');
+mongoose.Promise = require('bluebird');
+const server = express();
+server.use(express.static('../public'));
+server.set('views', __dirname + '/../views');
+server.use(bodyparser.urlencoded({extended: false}));
+server.engine('mustache', mustache());
+server.set('view engine', 'mustache');
+server.use(session({
+    secret: 'mjkhbki76gvftyjuu66bnA9ou7g',
+    resave: false,
+    saveUninitialized: true
+  }));
+server.get('/signup', function(req, res){
+    const states = [{value: "AL", state: "AL"}, {value: "AK", state: "AK"},
+     {value: "AZ", state: "AZ"},{value: "AR", state: "AR"}, {value: "CA", state: "CA"},
+    {value: "CO", state: "CO"}, {value: "CT", state: "CT"}, {value: "DE", state: "DE"},
+    {value: "FL", state: "FL"}, {value: "GA", state: "GA"}, {value: "HI", state: "HI"},
+    {value: "ID", state: "ID"}, {value: "IL", state: "IL"}, {value: "IN", state: "IN"},
+    {value: "CA", state: "CA"}, {value: "CA", state: "CA"}, {value: "CA", state: "CA"},
+    {value: "IA", state: "IA"}, {value: "KS", state: "KS"}, {value: "KY", state: "KY"},
+    {value: "LA", state: "LA"}, {value: "ME", state: "ME"}, {value: "MD", state: "MD"},
+    {value: "MA", state: "MA"}, {value: "MI", state: "MI"}, {value: "MN", state: "MN"},
+    {value: "MS", state: "MS"}, {value: "MO", state: "MO"}, {value: "MT", state: "MT"},
+    {value: "NE", state: "NE"}, {value: "NV", state: "NV"}, {value: "NH", state: "NH"},
+    {value: "NJ", state: "NJ"}, {value: "NM", state: "NM"}, {value: "NY", state: "NY"},
+    {value: "NC", state: "NC"}, {value: "ND", state: "ND"}, {value: "OH", state: "OH"},
+    {value: "OK", state: "OK"}, {value: "OR", state: "OR"}, {value: "PA", state: "PA"},
+    {value: "RI", state: "RI"}, {value: "SC", state: "SC"}, {value: "SD", state: "SD"},
+    {value: "TN", state: "TN"}, {value: "TX", state: "TX"}, {value: "UT", state: "UT"},
+    {value: "VT", state: "VT"}, {value: "VA", state: "VA"}, {value: "WA", state: "WA"},
+    {value: "WV", state: "WV"}, {value: "WI", state: "WI"}, {value: "WY", state: "WY"}];
+    res.render('signup', {
+      states: states,
+    });
+});
+server.post('/signup', function(req, res){
+    User.create({
+    username: req.body.username,
+    password: req.body.password,
+    reenterPassword: req.body.reenterPassword,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    city: req.body.city,
+    state: req.body.states,
+  })
+  .then(function(){
+    console.log("The user was added");
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+    res.render('login');
+});
+
+server.get('/login', function (req, res){
+    res.render('login');
+});
+
+server.post('/login', function(req, res) {
+  //Check out .populate
+  req.session.username = req.body.username;
+    User.findOne({username: req.body.username})
+      .then(function(data){
+        req.session.ObjectId = data._id;
+        console.log(data._id);
+        res.render('create', { // save the uername and id and cannot be done in the rendor
+        username: req.session.username,
+        user: req.session.ObjectId, //don't forget to pass the object id and username in the session
+        })
+      });
+});
+server.get('/create', function(req, res){
+  res.render('create', {
+    username: req.session.username,
+    user: req.session.ObjectId,
+
+  });
+  console.log(req.session.username, req.session.ObjectId);
+})
+server.post('/create', function(req, res){
+  Snippet.create({
+    coder: req.body.coder, title: req.body.title,
+    published_date: req.body.published_date, body: req.body.body,
+    notes: req.body.notes, language: req.body.language,
+    rating: req.body.rating, tags: req.body.tags,
+    user: req.session.ObjectId, ref: req.session.username,
+
+  })
+    .then(function(){
+        console.log("The snippet was added");
+        console.log(req.session.ObjectId);
+    })
+    .catch(function(){
+        //console.log(req.body.coder, req.body.title,req.body.published_date,req.body.body,req.body.language,req.body.notes,req.body.rating,req.body.tags, req.session.username);
+        console.log("The snippet wasn't added")
+    });
+      res.render('create');
+});
+
+server.get('/search', function(req, res){
+
+    res.render('search');
+});
+server.post('/search', function(req, res){
+  if (req.body.coder !== '' && req.body.language !== '') {
+     Snippet.find({ coder: req.body.coder, language: req.body.language })
+       .then(function(data){
+         res.render('search', {
+            returnedData: data
+         })
+       })
+       .catch(function(){
+       });
+   } else if (req.body.tags !== ''){
+      Snippet.find({tags: req.body.tags})
+        .then(function(data){
+          res.render('search', {
+          returnedData: data
+          });
+        console.log(req.body.tags);
+          console.log(data);
+        })
+        .catch(function(){
+          console.log("The search didn't work!");
+        });
+    } else if( req.body.coder !== '') {
+      console.log('this is a test');
+        Snippet.find({coder: req.body.coder})
+          .then(function(data){
+            res.render('search', {
+            returnedData: data
+            });
+            console.log("I should be printing" + req.body.coder);
+            // console.log(data);
+          })
+          .catch(function(){
+            console.log("The search didn't work!");
+          });
+       } else if (req.body.language !== ''){
+          Snippet.find({tags: req.body.language})
+            .then(function(data){
+              res.render('search', {
+              returnedData: data
+              });
+                console.log(req.body.language);
+                // console.log(data);
+            })
+            .catch(function(){
+              console.log("The language search didn't work!");
+            });
+         } else if( req.body.published_date !== '') {
+            Snippet.find({coder: req.body.published_date})
+              .then(function(data){
+                res.render('search', {
+                returnedData: data
+                });
+                  console.log(req.body.published_date);
+                  // console.log(data);
+              })
+              .catch(function(){
+                console.log("The search didn't work!");
+              });
+            } else if (req.body.title !== ''){
+                Snippet.find({tags: req.body.title})
+                  .then(function(data){
+                    res.render('search', {
+                    returnedData: data
+                    });
+                      console.log(req.body.title);
+                      // console.log(data);
+                  })
+                  .catch(function(){
+                    console.log("The language search didn't work!");
+                  });
+            } else if (req.body.title !== ''){
+                Snippet.find({tags: req.body.title})
+                  .then(function(data){
+                    res.render('search', {
+                    returnedData: data
+                    });
+                      console.log(req.body.title);
+                      // console.log(data);
+                  })
+                  .catch(function(){
+                    console.log("The language search didn't work!");
+                  });
+              };
+    });
+server.get('/edit', function(req, res){
+  res.render('edit');
+})
+server.post('/edit', function(req, res){
+  res.render('edit');
+})
+
+server.get('/snippet', function(req, res){
+  res.render('index');
+})
+
+server.listen(3300, function(req, res){
+  console.log("The server is running, booyahh");
+});
